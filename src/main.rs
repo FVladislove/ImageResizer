cd668d1bd78052c9852d5eebef7ae35e0d1524b1
Elis <f.vlad.is.love@gmail.com>
2025-02-14 11:47:15 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+use std::ffi::OsStr;
+use image::imageops::FilterType;
+use walkdir;
+use walkdir::{DirEntry, WalkDir};
+
+fn is_image(file: &DirEntry, image_formats: &[&str]) -> bool {
+    image_formats.contains(&file.path().extension()
+        .unwrap_or(OsStr::new(""))
+        .to_str().unwrap())
+}
+
+fn main() {
+    let image_formats = ["png", "jpg", "jpeg"];
+
+    for e in WalkDir::new(".").into_iter().filter_map(|e| e.ok()) {
+        if e.metadata().unwrap().is_file() && is_image(&e, &image_formats){
+            println!("{}", e.path().display());
+
+            let image = image::open(e.path()).unwrap();
+
+            let width = image.width() as f32;
+            let height = image.height() as f32;
+
+            if width.max(height) < 2000_f32 {
+                continue;
+            }
+
+            let resize_coefficient: f32 = 2000_f32 / width.max(height);
+
+
+            let resized_image = image.resize(
+                (width * resize_coefficient) as u32,
+                (height * resize_coefficient) as u32,
+                FilterType::Lanczos3);
+
+            let path = e.path().to_str().unwrap();
+
+            // let (path_with_filename, extension) = path.split_at(path.rfind(".").unwrap());
+            // for debug purposes
+            // let output_path = [path_with_filename, "-v2", extension].concat();
+            resized_image.save(path).expect("TODO: panic message");
+        }
+    }
+}
