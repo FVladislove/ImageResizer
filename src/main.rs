d86bf52da86058a920fcc03332e676a63b656061
Elis <f.vlad.is.love@gmail.com>
2025-02-15 04:28:02 +0200

    Added some perf tests and minor performance improvement

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
+use std::cmp::max;
 use std::ffi::OsStr;
 use std::time::Instant;
-use image::imageops;
+use image::{imageops, ImageReader};
 use image::imageops::FilterType;
 use walkdir;
 use walkdir::{WalkDir};
@@ -16,24 +17,22 @@ fn main() {
         if e.metadata().unwrap().is_file()
             && image_formats.iter().any(|&format| extension == format) {
 
-            let image = image::open(e.path()).unwrap();
+            let img_reader = ImageReader::open(e.path()).unwrap();
+            let (width, height) = img_reader.into_dimensions().unwrap();
 
-            let width = image.width() as f32;
-            let height = image.height() as f32;
-
-            if width.max(height) < 2000_f32 {
+            if max(width, height) < 2000 {
                 continue;
             }
 
-            let resize_coefficient: f32 = 2000_f32 / width.max(height);
+            let image = image::open(e.path()).unwrap();
+
+            let resize_coefficient: f32 = 2000_f32 / max(width, height) as f32;
             let resized_image = image.resize(
-                (width * resize_coefficient) as u32,
-                (height * resize_coefficient) as u32,
+                (width as f32 * resize_coefficient) as u32,
+                (height as f32 * resize_coefficient) as u32,
                 FilterType::Lanczos3);
 
-            let path = e.path().to_str().unwrap();
-
-            resized_image.save(path).expect("TODO: panic message");
+            resized_image.save(e.path()).expect("TODO: panic message");
         }
     }
     println!("Took: {:?}", now.elapsed());
