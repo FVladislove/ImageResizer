16c70dda008715057a891b0e841586100c47504f
Elis <f.vlad.is.love@gmail.com>
2025-02-17 11:55:54 +0200

    Added time measurements for different operations

--- resize.py
+++ resize.py
@@ -14,8 +14,18 @@ def initialize_logging():
         filemode='a'  # Append to the log file (use 'w' to overwrite)
     )
 
-
+i = 0
+extension_calc = 0
+image_opening = 0
+image_resizing = 0
+image_saving = 0
 def convert_images(folder_path: str, level=1):
+    global i
+    global extension_calc
+    global image_opening
+    global image_resizing
+    global image_saving
+
     print("".join(["\t" * level, "L-- ", folder_path]))
 
     for filename in os.listdir(folder_path):
@@ -23,9 +33,12 @@ def convert_images(folder_path: str, level=1):
         if os.path.isdir(item_path):
             convert_images(item_path, level + 1)
 
-        if filename.endswith((".jpg", ".png")):
+        if filename.endswith((".jpg", ".png", ".jpeg")):
             try:
+                i += 1
+                start = time.time_ns()
                 image = Image.open(item_path)
+                image_opening += time.time_ns() - start
             except FileNotFoundError as e:
                 initialize_logging()
                 logging.error(f"File not found: {item_path}  {e}")
@@ -43,18 +56,34 @@ def convert_images(folder_path: str, level=1):
                 image.close()
                 continue
 
+            start = time.time_ns()
             resize_coefficient = 2000 / max(width, height)
+            extension_calc += time.time_ns() - start
 
-            image.resize((int(width * resize_coefficient), int(height * resize_coefficient)), resample=Resampling.LANCZOS).save(item_path)
+            start = time.time_ns()
+            resized_image = image.resize(
+                (int(width * resize_coefficient), int(height * resize_coefficient)),
+                resample=Resampling.LANCZOS)
+            image_resizing = time.time_ns() - start
+
+            start = time.time_ns()
+            resized_image.save(item_path)
+            image_saving = time.time_ns() - start
             image.close()
 
 try:
     script_path = os.path.abspath(__file__)
     script_dir = os.path.dirname(script_path)
+    script_dir += "/test_folder"
     start = time.time_ns()
     convert_images(script_dir)
     end = time.time_ns()
-    print((end - start) / 1000000)
+    print(f"Total images processed:\t{i}")
+    print(f"Image opening mean:\t{image_opening / 1e6}ms")
+    print(f"Extension calc mean:\t{extension_calc / 1e6}ms")
+    print(f"Image resizing mean:\t{image_resizing / 1e6}ms")
+    print(f"Image saving mean:\t{image_saving / 1e6}ms")
+    print(f"Total:\t{(end - start) / 1e6}ms")
 # bad style, but I haven't time (and desire) to write tests
 # and check all possible errors
 except Exception as e:
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::cmp::max;
 use std::ffi::OsStr;
-use std::time::Instant;
+use std::time::{Duration, Instant};
 use image::{ImageReader};
 use image::imageops::FilterType;
 use walkdir;
@@ -9,16 +9,33 @@ use walkdir::{WalkDir};
 fn main() {
     let image_formats = ["png", "jpg", "jpeg"];
     let now = Instant::now();
-    for e in WalkDir::new(".").into_iter().filter_map(|e| e.ok()) {
+    let mut local_now: Instant;
+
+    let mut extension_calc = Duration::from_millis(0);
+    let mut reader_opening = Duration::from_millis(0);
+    let mut image_opening = Duration::from_millis(0);
+    let mut image_resizing = Duration::from_millis(0);
+    let mut image_saving = Duration::from_millis(0);
+    let mut i = 0;
+
+    for e in WalkDir::new("./test_folder").into_iter().filter_map(|e| e.ok()) {
         if e.metadata().unwrap().is_dir() {
             println!("{}", e.path().display());
             continue;
         }
 
+        local_now = Instant::now();
         let extension = e.path().extension().unwrap_or(OsStr::new(""));
+        extension_calc += local_now.elapsed();
 
         if image_formats.iter().any(|&format| extension == format) {
+            i += 1;
+            println!("\t|-- {}", e.path().file_name().unwrap().to_str().unwrap());
+
+            local_now = Instant::now();
             let img_reader = ImageReader::open(e.path()).unwrap();
+            reader_opening += local_now.elapsed();
+
             let (width, height) = img_reader.into_dimensions().unwrap();
 
             if max(width, height) <= 2000 {
@@ -27,14 +44,28 @@ fn main() {
 
             let resize_coefficient: f32 = 2000_f32 / max(width, height) as f32;
 
+            local_now = Instant::now();
             let image = image::open(e.path()).unwrap();
+            image_opening += local_now.elapsed();
+
+            local_now = Instant::now();
             let resized_image = image.resize(
                 (width as f32 * resize_coefficient) as u32,
                 (height as f32 * resize_coefficient) as u32,
                 FilterType::Lanczos3);
+            image_resizing += local_now.elapsed();
 
+            local_now = Instant::now();
             resized_image.save(e.path()).expect("TODO: panic message");
+            image_saving += local_now.elapsed();
         }
     }
-    println!("Took: {:?}", now.elapsed());
+
+    println!("Total images processed:\t{:?}", i);
+    println!("Extension calc mean:\t{:?}", extension_calc / i);
+    println!("Reader opening mean:\t{:?}", reader_opening / i);
+    println!("Image opening mean:\t{:?}", image_opening / i);
+    println!("Image resizing mean:\t{:?}", image_resizing / i);
+    println!("Image saving mean:\t{:?}", image_saving / i);
+    println!("Total:\t{:?}", now.elapsed());
 }
